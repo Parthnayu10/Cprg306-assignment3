375abc21a65c03a541d4cdc8213109d35e101d11
Parthnayu10 <123720524+Parthnayu10@users.noreply.github.com>
2023-10-02 04:22:06 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image  from 'next/image';
+ import Image  from 'next/image';
 import Link from 'next/link'; // Import Link from Next.js
 
 export default function Home() {
@@ -7,10 +7,8 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
-         <p> <code className="font-mono font-bold">cprg306-assignment</code></p>
-         <p> <code className='font-mono font-normal'>Parth Patel</code></p>
-         <p> <code className='font-mono font-normal'>cprg306C</code></p>
-          <p><code className='font-mono font-normal'>Github repsiratory</code></p>
+          <code className="font-mono font-bold">cprg306-assignment</code>
+         
           
         </p>
       </div>
