a8e5ee88866c1ed9ab28a7b2a17008ee8dcbb99d
Parthnayu10 <123720524+Parthnayu10@users.noreply.github.com>
2023-11-06 02:01:40 -0700

    update

--- app/week8/_utils/auth-context.js
+++ app/week8/_utils/auth-context.js
@@ -1,5 +1,5 @@
-"use client"; // This comment is not a recognized directive, you can remove it.
-
+"use client";
+ 
 import { useContext, createContext, useState, useEffect } from "react";
 import {
   signInWithPopup,
@@ -8,42 +8,35 @@ import {
   GithubAuthProvider,
 } from "firebase/auth";
 import { auth } from "./firebase";
-
-// Create the AuthContext
+ 
 const AuthContext = createContext();
-
-// AuthContextProvider component
+ 
 export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-
-  // Function for GitHub sign-in
+ 
   const gitHubSignIn = () => {
     const provider = new GithubAuthProvider();
     return signInWithPopup(auth, provider);
   };
-
-  // Function for signing out
+ 
   const firebaseSignOut = () => {
     return signOut(auth);
   };
-
+ 
   useEffect(() => {
-    // Listen for auth state changes
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
     });
-    
-    return () => unsubscribe(); // Unsubscribe when the component unmounts
-  }, []); // Pass an empty dependency array to run the effect once
-
+    return () => unsubscribe();
+  }, [user]);
+ 
   return (
     <AuthContext.Provider value={{ user, gitHubSignIn, firebaseSignOut }}>
       {children}
     </AuthContext.Provider>
   );
 };
-
-// Custom hook for accessing user and auth functions
+ 
 export const useUserAuth = () => {
   return useContext(AuthContext);
 };
--- app/week8/page.js
+++ app/week8/page.js
@@ -1,15 +1,29 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUserAuth } from '../week8/_utils/auth-context';
-
 
 const LandingPage = () => {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const [popupClosed, setPopupClosed] = useState(false);
 
   // Function to handle GitHub sign-in
   const handleGitHubSignIn = async () => {
-    await gitHubSignIn();
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      if (error.code === 'auth/popup-closed-by-user') {
+        // Handle the popup closed by the user
+        setPopupClosed(true);
+      } else {
+        // Handle other authentication errors here
+        console.error(error);
+      }
+    }
   };
+
+  useEffect(() => {
+    // Your code that uses user, gitHubSignIn, or firebaseSignOut can go here.
+  }, []); // Ensure this code runs on the client-side
 
   const shoppingListStyle = {
     fontSize: '2rem', // Adjust the size as needed
@@ -23,13 +37,14 @@ const LandingPage = () => {
   return (
     <div>
       <p style={shoppingListStyle}>ShoppingList APP {user ? user.displayName : ''}</p>
+      {popupClosed && <p>The sign-in popup was closed by the user.</p>}
       {user ? (
         <>
           <p>
             Welcome, {user.displayName} ({user.email})
           </p>
           <button onClick={handleLogOut}>Log Out</button>
-          <a href="/week8/shopping-list">Go to Shopping List</a>
+          <a href="/shopping-list">Go to Shopping List</a>
         </>
       ) : (
         <button onClick={handleGitHubSignIn}>Sign in with GitHub</button>
